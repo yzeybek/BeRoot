3306558f4f00ae632786d0d543b0efdc5246e795
Yasin Zeybek <yzeybek@k1m09s07.42istanbul.com.tr>
2024-12-05 13:10:03 +0300

    basics handled

--- scripts/basics.bash
+++ scripts/basics.bash
@@ -6,14 +6,12 @@ gnome-extensions enable ubuntu-dock@ubuntu.com
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
 gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
 
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
-gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
+#gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+#gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+#gsettings set org.gnome.desktop.interface icon-theme 'Yaru'
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$PROFILE_ID/ use-custom-command true
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$PROFILE_ID/ custom-command 'docker exec -it BeRoot /usr/bin/bash'
-gsettings set org.gnome.shell.extensions.ding show-home false
-gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
+#gsettings set org.gnome.shell.extensions.ding show-home false
+#gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
 
-nmcli connection down "Wired connection 2"
-
-echo 'xrandr --output eDP --mode 3200x1800' > ~/.profile
+#echo 'xrandr --output eDP --mode 3200x1800' > ~/.profile
